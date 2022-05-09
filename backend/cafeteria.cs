9374a56b9f2b9daa73ed6ef4823cb1919cff40a1
DESKTOP-AVKJAFG\Haitian Ji <haji@floridauniversitaria.es>
2022-05-09 19:44:49 +0200

    backend dia1

---
+++ backend/cafeteria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoIntegrado
+{
+    internal class cafeteria
+    {
+        private int numero_mesa;
+        private bool reservado;
+
+        public cafeteria(int numero_mesa, bool reservado)
+        {
+            this.numero_mesa = numero_mesa;
+            this.reservado = reservado;
+        }
+
+        public int Numero_mesa { get => numero_mesa; set => numero_mesa = value; }
+        public bool Reservado { get => reservado; set => reservado = value; }
+    }
+}
---
+++ backend/conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AEV7
+{
+    internal class conexion
+    {
+        
+        ///Las conexiones a la base de datos se realizan mediante el metodo Singleton, utilizando esta clase.
+
+        private static MySqlConnection instancia = null;
+        private static readonly object padlock = new object();
+
+
+        private conexion() { }
+
+
+        public static MySqlConnection Conexion
+        {
+            get
+            {
+                lock (padlock)
+                {   
+                    if (instancia == null)
+                    {
+                        instancia = new MySqlConnection();
+                        string server = "server=127.0.0.1;";
+                        string port = "port=3306;";
+                        string database = "database=bd_gestion_empleados;";
+                        string usuario = "uid=root;";
+                        string password = "pwd=;";
+                        instancia.ConnectionString = server + port + database + usuario + password;
+                    }
+                    return instancia;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Abre la conexión a la base de datos.
+        /// </summary>
+          public static void AbrirConexion()
+        {
+            if (instancia != null)
+                instancia.Open();
+        }
+
+        /// <summary>
+        /// Cierra la conexión a la base de datos.
+        /// </summary>
+        public static void CerrarConexion()
+        {
+            if (instancia != null)
+                instancia.Close();
+        }
+    }
+}
