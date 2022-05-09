250fefc5fd5cbdd1aeb19f9e3eebe85a72e25d76
Gabriel Argente <gaarag@floridauniversitaria.es>
2022-05-09 20:04:18 +0200

    Front end día 1

---
+++ floridapp/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace floridapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnCarouReunion = new System.Windows.Forms.Button();
+            this.btnCarouCafeteria = new System.Windows.Forms.Button();
+            this.btnCarouBiblioteca = new System.Windows.Forms.Button();
+            this.btnCarouNews = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.lblTitulo);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 804);
+            this.panel1.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(84, 201);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(108, 23);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "FLORIDAPP";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(278, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1108, 123);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(319, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(507, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NUESTROS SERVICIOS";
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Image = global::floridapp.Properties.Resources._2799204_management_business_office_setting_success_icon;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(0, 722);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(278, 79);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "   Salida";
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = global::floridapp.Properties.Resources._2799204_management_business_office_setting_success_icon;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(0, 507);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(278, 79);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "   Florida News";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = global::floridapp.Properties.Resources._2799204_management_business_office_setting_success_icon;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(0, 426);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(278, 79);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "   Biblioteca";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::floridapp.Properties.Resources._2799204_management_business_office_setting_success_icon;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(0, 345);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(278, 79);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "   Cafetería";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::floridapp.Properties.Resources.floridapng;
+            this.pictureBox1.Location = new System.Drawing.Point(63, 46);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = global::floridapp.Properties.Resources._2799204_management_business_office_setting_success_icon;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 263);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "   Reunion";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Controls.Add(this.btnCarouNews);
+            this.panel3.Controls.Add(this.btnCarouBiblioteca);
+            this.panel3.Controls.Add(this.btnCarouCafeteria);
+            this.panel3.Controls.Add(this.btnCarouReunion);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(278, 704);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1108, 100);
+            this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // btnCarouReunion
+            // 
+            this.btnCarouReunion.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCarouReunion.Location = new System.Drawing.Point(229, 31);
+            this.btnCarouReunion.Name = "btnCarouReunion";
+            this.btnCarouReunion.Size = new System.Drawing.Size(116, 40);
+            this.btnCarouReunion.TabIndex = 1;
+            this.btnCarouReunion.Text = "Reunión";
+            this.btnCarouReunion.UseVisualStyleBackColor = true;
+            // 
+            // btnCarouCafeteria
+            // 
+            this.btnCarouCafeteria.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCarouCafeteria.Location = new System.Drawing.Point(406, 31);
+            this.btnCarouCafeteria.Name = "btnCarouCafeteria";
+            this.btnCarouCafeteria.Size = new System.Drawing.Size(116, 40);
+            this.btnCarouCafeteria.TabIndex = 2;
+            this.btnCarouCafeteria.Text = "Cafetería";
+            this.btnCarouCafeteria.UseVisualStyleBackColor = true;
+            // 
+            // btnCarouBiblioteca
+            // 
+            this.btnCarouBiblioteca.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCarouBiblioteca.Location = new System.Drawing.Point(588, 31);
+            this.btnCarouBiblioteca.Name = "btnCarouBiblioteca";
+            this.btnCarouBiblioteca.Size = new System.Drawing.Size(116, 40);
+            this.btnCarouBiblioteca.TabIndex = 3;
+            this.btnCarouBiblioteca.Text = "Biblioteca";
+            this.btnCarouBiblioteca.UseVisualStyleBackColor = true;
+            // 
+            // btnCarouNews
+            // 
+            this.btnCarouNews.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCarouNews.Location = new System.Drawing.Point(770, 31);
+            this.btnCarouNews.Name = "btnCarouNews";
+            this.btnCarouNews.Size = new System.Drawing.Size(116, 40);
+            this.btnCarouNews.TabIndex = 4;
+            this.btnCarouNews.Text = "Florida News";
+            this.btnCarouNews.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(278, 123);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1108, 581);
+            this.panel4.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1386, 804);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnCarouReunion;
+        private System.Windows.Forms.Button btnCarouNews;
+        private System.Windows.Forms.Button btnCarouBiblioteca;
+        private System.Windows.Forms.Button btnCarouCafeteria;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ floridapp/Usuario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace floridapp
+{
+    class Usuario
+    {
+        private string nif;
+        private string correo;
+        private string password;
+        private string nombre;
+        private string apellido;
+        private int tel;
+        private bool profesor;
+        private bool administrador;
+        private bool cocina;
+        private bool biblioteca;
+
+        public string Nif { get => nif; set => nif = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Password { get => password; set => password = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Tel { get => tel; set => tel = value; }
+        public bool Profesor { get => profesor; set => profesor = value; }
+        public bool Administrador { get => administrador; set => administrador = value; }
+        public bool Cocina { get => cocina; set => cocina = value; }
+        public bool Biblioteca { get => biblioteca; set => biblioteca = value; }
+
+        public Usuario(string nif, string correo, string password, string nombre, string apellido, int tel, bool profesor, bool administrador, bool cocina, bool biblioteca)
+        {
+            this.nif = nif;
+            this.correo = correo;
+            this.password = password;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.tel = tel;
+            this.profesor = profesor;
+            this.administrador = administrador;
+            this.cocina = cocina;
+            this.biblioteca = biblioteca;
+        }
+
+        public Usuario()
+        {
+
+        }
+    }
+}
