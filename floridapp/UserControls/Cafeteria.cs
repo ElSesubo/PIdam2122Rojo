fe46022c089aadf744e16e6c7d11321416ba4368
Gabriel Argente <gaarag@floridauniversitaria.es>
2022-05-10 19:30:14 +0200

    Frontend día 2

---
+++ floridapp/UserControls/Cafeteria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace floridapp
+{
+    public partial class Cafeteria : UserControl
+    {
+        public Cafeteria()
+        {
+            InitializeComponent();
+        }
+
+        private void Cafeteria_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
