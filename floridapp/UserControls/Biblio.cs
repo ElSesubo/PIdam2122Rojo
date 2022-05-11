a774bfd66bfaf43dfe4aa643f59ef315d137759f
Gabriel Argente <gaarag@floridauniversitaria.es>
2022-05-11 16:56:41 +0200

    Front end día 3 en proceso

---
+++ floridapp/UserControls/Biblio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace floridapp.UserControls
+{
+    public partial class Biblio : UserControl
+    {
+        public Biblio()
+        {
+            InitializeComponent();
+        }
+
+        private void Biblioteca_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- floridapp/UserControls/Cafeteria.Designer.cs
+++ floridapp/UserControls/Cafeteria.Designer.cs
@@ -35,8 +35,8 @@ namespace floridapp
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +45,7 @@ namespace floridapp
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Broadway", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(33)))), ((int)(((byte)(47)))));
-            this.label1.Location = new System.Drawing.Point(60, 196);
+            this.label1.Location = new System.Drawing.Point(91, 196);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(393, 109);
             this.label1.TabIndex = 0;
@@ -55,7 +55,7 @@ namespace floridapp
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(89, 305);
+            this.label2.Location = new System.Drawing.Point(120, 305);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(176, 32);
             this.label2.TabIndex = 1;
@@ -65,7 +65,7 @@ namespace floridapp
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(92, 351);
+            this.label3.Location = new System.Drawing.Point(123, 351);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(228, 18);
             this.label3.TabIndex = 2;
@@ -75,7 +75,7 @@ namespace floridapp
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(92, 383);
+            this.label4.Location = new System.Drawing.Point(123, 383);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(504, 120);
             this.label4.TabIndex = 3;
@@ -86,33 +86,33 @@ namespace floridapp
             this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(95, 506);
+            this.button1.Location = new System.Drawing.Point(126, 506);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(380, 37);
             this.button1.TabIndex = 4;
             this.button1.Text = "Realizar Pedido";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::floridapp.Properties.Resources.Receta_paella_Valenciana;
-            this.pictureBox1.Location = new System.Drawing.Point(557, 102);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(750, 750);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(33)))), ((int)(((byte)(47)))));
-            this.label5.Location = new System.Drawing.Point(41, 194);
+            this.label5.Location = new System.Drawing.Point(72, 194);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(141, 19);
             this.label5.TabIndex = 6;
             this.label5.Text = "Por solamente:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::floridapp.Properties.Resources.Receta_paella_Valenciana;
+            this.pictureBox1.Location = new System.Drawing.Point(537, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(750, 750);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // Cafeteria
             // 
