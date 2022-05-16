4d60d3132e56ae529f23e826283434014024f98a
ALUMNADO\dacoma <dacoma@floridauniversitaria.es>
2022-05-16 19:55:43 +0200

    actualizacion accesococina

---
+++ floridapp/Pedido.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace floridapp
+{
+    class Pedido
+    {
+        private int id;
+        private bool llevar;
+        private string nif;
+        private TimeSpan hora;
+        private bool recogido;
+        private bool preparado;
+        private string menu;
+
+        public Pedido(int id, bool llevar, string nif, TimeSpan hora, bool recogido, bool preparado, string menu )
+        {
+            this.id = id;
+            this.llevar = llevar;
+            this.hora = hora;
+            this.recogido = recogido;
+            this.preparado = preparado;
+            this.nif = nif;
+            this.menu = menu;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public bool Llevar { get => llevar; set => llevar = value; }
+        public TimeSpan Hora { get => hora; set => hora = value; }
+        public bool Recogido { get => recogido; set => recogido = value; }
+        public bool Preparado { get => preparado; set => preparado = value; }
+        public string Nif { get => nif; set => nif = value; }
+        public string Menu { get => menu; set => menu = value; }
+
+        public static void InsertarReserva(TimeSpan reserva, string menu, string nif, bool llevar)
+        {
+            string consulta = "";
+            consulta = String.Format("INSERT INTO pedido (hora_pedido, menu, nifusu, llevar, recogido, preparado) VALUES " +
+                    "('{0}','{1}','{2}','{3}',0,0)", reserva, menu, nif,llevar);
+
+            MySqlCommand comando = new MySqlCommand(consulta, conexion.Conexion);
+            comando.ExecuteReader();
+
+        }
+        public static List<Pedido> ListaMenu()
+        {
+            List<Pedido> lista = new List<Pedido>();
+            string consulta = "";
+            consulta = String.Format("SELECT id, llevar, nifusu, hora_pedido, recogido, preparado, menu FROM pedido WHERE recogido=0;");
+            MySqlCommand comando = new MySqlCommand(consulta, conexion.Conexion);
+            MySqlDataReader reader = comando.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Pedido pedi = new Pedido(reader.GetInt16(0), reader.GetBoolean(1), reader.GetString(2),reader.GetTimeSpan(3),reader.GetBoolean(4),reader.GetBoolean(5),reader.GetString(6));
+                    lista.Add(pedi);
+                }
+            }
+            reader.Close();
+            return lista;
+        }
+
+        public static void ActualizarPreparado(int id)
+        {
+            string consulta2 = "";
+            consulta2 = String.Format("UPDATE pedido set preparado=1 where id='{0}';", id);
+            MySqlCommand comando2 = new MySqlCommand(consulta2, conexion.Conexion);
+            comando2.ExecuteReader();
+        }
+
+        public static void ActualizarRecogido(int id)
+        {
+
+                string consulta2 = "";
+                consulta2 = String.Format("UPDATE pedido set recogido=1 where id='{0}';", id);
+                MySqlCommand comando2 = new MySqlCommand(consulta2, conexion.Conexion);
+                comando2.ExecuteReader();
+
+ 
+        }
+        public static List<Pedido> BuscarMenu(string nif)
+        {
+            List<Pedido> lista = new List<Pedido>();
+            string consulta = "";
+            consulta = String.Format("SELECT id, llevar, nifusu, hora_pedido, recogido, preparado, menu FROM pedido WHERE nifusu='{0}' AND recogido=0;",nif);
+            MySqlCommand comando = new MySqlCommand(consulta, conexion.Conexion);
+            MySqlDataReader reader = comando.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Pedido pedi = new Pedido(reader.GetInt16(0), reader.GetBoolean(1), reader.GetString(2), reader.GetTimeSpan(3), reader.GetBoolean(4), reader.GetBoolean(5), reader.GetString(6));
+                    lista.Add(pedi);
+                }
+            }
+            reader.Close();
+            return lista;
+        }
+
+    }
+}
--- floridapp/accesoCocina.cs
+++ floridapp/accesoCocina.cs
@@ -27,6 +27,7 @@ namespace floridapp
         {
 
             Cargar();
+            Cargar2();
 
         }
 
@@ -79,10 +80,117 @@ namespace floridapp
                 dtgvPedidos.Rows.Add(cafe[i].Numero_mesa, cafe[i].Ocupado);
             }
         }
+        private void Cargar2()
+        {
+            List<Pedido> pedi = new List<Pedido>();
 
+            if (conexion.Conexion != null)
+            {
+                conexion.AbrirConexion();
+                pedi = Pedido.ListaMenu();
+                conexion.CerrarConexion();
+            }
+            dgvPedidoMenu.Rows.Clear();
+            for (int i = 0; i < pedi.Count; i++)
+            {
+                dgvPedidoMenu.Rows.Add(pedi[i].Id, pedi[i].Llevar, pedi[i].Nif, pedi[i].Hora, pedi[i].Recogido, pedi[i].Preparado, pedi[i].Menu);
+            }
+        }
         private void dtgvPedidos_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void dgvPedidoMenu_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnPreparado_Click(object sender, EventArgs e)
+        {
+            if (dgvPedidoMenu.SelectedRows.Count == 1)
+            {
+                int id = (Convert.ToInt32(dgvPedidoMenu.CurrentRow.Cells[0].Value));
+
+
+
+                DialogResult confirmacion = MessageBox.Show("Menu preparado en el registro seleccionado. ¿Continuar?",
+                                                    "Eliminación", MessageBoxButtons.YesNo);
+
+                if (confirmacion == DialogResult.Yes)
+                {
+                    if (conexion.Conexion != null)
+                    {
+                        conexion.AbrirConexion();
+                        Pedido.ActualizarPreparado(id);
+                        conexion.CerrarConexion();
+                    }
+                    Cargar2();
+                }
+            }
+        }
+
+        private void btnEliminarMenu_Click(object sender, EventArgs e)
+        {
+            if (dgvPedidoMenu.SelectedRows.Count == 1)
+            {
+                int id = (Convert.ToInt32(dgvPedidoMenu.CurrentRow.Cells[0].Value));
+                bool preparado = (Convert.ToBoolean(dgvPedidoMenu.CurrentRow.Cells[5].Value));
+
+                if (preparado == true)
+                {
+                    DialogResult confirmacion = MessageBox.Show("Menu recogido en el registro seleccionado. ¿Continuar?",
+                                    "Eliminación", MessageBoxButtons.YesNo);
+
+                    if (confirmacion == DialogResult.Yes)
+                    {
+                        if (conexion.Conexion != null)
+                        {
+                            conexion.AbrirConexion();
+                            Pedido.ActualizarRecogido(id);
+                            conexion.CerrarConexion();
+                        }
+                        Cargar2();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Aun no esta preparado");
+                }
+
+
+            }
+        }
+
+        private void btnBuscarMenu_Click(object sender, EventArgs e)
+        {
+            List<Pedido> pedi2 = new List<Pedido>();
+            string nif = "";
+            nif = txtBuscarMenu.Text;
+            
+            for(int i = 0; i < dgvPedidoMenu.Rows.Count; i++)
+            {
+                if (nif ==Convert.ToString(dgvPedidoMenu.Rows[i].Cells[2].Value))
+                {
+                    if (conexion.Conexion != null)
+                    {
+                        conexion.AbrirConexion();
+                        pedi2=Pedido.BuscarMenu(nif);
+                        conexion.CerrarConexion();
+  
+                    }
+                    dgvPedidoMenu.Rows.Clear();
+                    for (int j = 0; j < pedi2.Count; j++)
+                    {
+                        dgvPedidoMenu.Rows.Add(pedi2[j].Id, pedi2[j].Llevar, pedi2[j].Nif, pedi2[j].Hora, pedi2[j].Recogido, pedi2[j].Preparado, pedi2[j].Menu);
+                    }
+                }
+            }
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            Cargar2();
         }
     }
 }
